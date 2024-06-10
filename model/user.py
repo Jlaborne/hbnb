1148af7d7aa30d4c07206598ee83fa9a77b6ae95
Jlaborne <147745163+Jlaborne@users.noreply.github.com>
2024-06-10 13:45:57 +0200

    Update user.py

--- model/user.py
+++ model/user.py
@@ -1,9 +1,12 @@
 class User:
     def __init__(self, email, password, first_name, last_name):
+        self.id = uuid.uuid4()
         self.email = email
         self.password = password
         self.first_name = first_name
         self.last_name = last_name
+        self.created_at = datetime.now()  # Creation Date
+        self.updated_at = self.created_at  # Update Date
         self.reviews = []
 
     def add_review(self, review):
