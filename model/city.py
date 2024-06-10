2cd3d068e2447e5296bc10b43b12a2d1998bebcf
Jlaborne <147745163+Jlaborne@users.noreply.github.com>
2024-06-10 13:04:34 +0200

    Update city.py

--- model/city.py
+++ model/city.py
@@ -1,23 +1,30 @@
-import uuid
-from datetime import datetime
-from persistence.storage import Storage
+from persistence.data_manager import DataManager
 
 class City:
-    storage = Storage()
+    data_manager = DataManager()
 
     def __init__(self, name, country_code):
-        self.id = str(uuid.uuid4())
+        self.id = None
         self.name = name
         self.country_code = country_code
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        City.storage.add_city(self)
+        self.created_at = None
+        self.updated_at = None
 
-    def update_city(self, **kwargs):
+    def save(self):
+        return City.data_manager.save(self)
+
+    def update(self, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
-        self.updated_at = datetime.now()
-        City.storage.update_city(self)
+        return City.data_manager.update(self)
 
-    def delete_city(self):
-        City.storage.delete_city(self.id)
+    def delete(self):
+        return City.data_manager.delete(self.id, 'city')
+
+    @classmethod
+    def get(cls, city_id):
+        return City.data_manager.get(city_id, 'city')
+
+    @classmethod
+    def get_all(cls):
+        return City.data_manager.get_all('city')
