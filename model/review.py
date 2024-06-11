982072433643f35779dfc19928aedbfaf44299b0
Jlaborne <147745163+Jlaborne@users.noreply.github.com>
2024-06-11 12:51:21 +0200

    Update review.py

--- model/review.py
+++ model/review.py
@@ -2,10 +2,13 @@ import uuid
 from datetime import datetime
 
 class Review:
-    def __init__(self, rating, comment, user):
-        self.id = uuid.uuid4()  # Unique ID
-        self.rating = rating
-        self.comment = comment
-        self.user = user
-        self.created_at = datetime.now()  # Creation Date
-        self.updated_at = self.created_at  # Update Date
+    def __init__(self, user_id, place_id, content):
+        self.id = uuid.uuid4()
+        self.user_id = user_id
+        self.place_id = place_id
+        self.content = content
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        return f"Review by User {self.user_id} for Place {self.place_id}: {self.content}"
