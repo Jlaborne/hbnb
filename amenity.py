b8823ee182026891ab9d5a59f83d4acc43b13e05
Jlaborne <147745163+Jlaborne@users.noreply.github.com>
2024-06-11 12:45:02 +0200

    Create amenity.py

---
+++ amenity.py
@@ -0,0 +1,9 @@
+class Amenity:
+    def __init__(self, name):
+        self.id = uuid.uuid4()
+        self.name = name
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        return f"Amenity: {self.name}"
